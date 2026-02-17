740feed8cb514c84d02820fc5d9d94cb104899ef
clari <clari@DESKTOP-10K93MB>
2026-02-17 12:17:16 -0600

    Add project files.

---
+++ CowData.h
@@ -0,0 +1,20 @@
+//need to pull the cow stuff from the livestock tracker project for this project
+//Can make it however I want
+
+//Cow Class Header File
+
+#ifndef COWDATA_H
+#define COWDATA_H
+
+#include <iostream>
+
+struct Cow
+{
+
+};
+
+
+
+
+
+#endif
